ad5773bb3d3d5500a5c695dacace5c6e511073d0
deandradiedrick <dadiedrick@gmail.com>
2023-06-22 21:40:45 -0700

    Create schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS catchUp_db;
+CREATE DATABASE catchUp_db;
+
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(30)
+  
+)
