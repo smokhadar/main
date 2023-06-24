f1aaa165fd0d0a67b621c23171321be6df9ae17b
shahnidhi20 <shahnidhi20@gmail.com>
2023-06-24 12:17:46 -0700

    load the login page handler as default

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const exphbs = require("express-handlebars");
+const hbs = require("express-handlebars");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
@@ -9,11 +9,23 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
+//const hbs = exphbs.create({ helpers });
+app.set("view engine", "hbs");
 // Inform Express.js on which template engine to use
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutDir: __dirname + "/views/layouts/",
+  })
+);
+
+//main page to render
+app.get("/", (req, res) => {
+  console.log("Loading");
+  res.render("login", { layout: "index" });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
