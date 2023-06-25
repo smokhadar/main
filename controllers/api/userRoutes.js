39159dc5f10e0f5f0dfacb8e4f808cdf59986eb8
sarahm <minutemin@gmail.com>
2023-06-24 21:47:42 -0700

    added in comments get all route for all users for testing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// router.get("/users", async (req, res) => {
+//   try {
+//     const userData = await User.findAll(req.body);
+//     res.status(200).json(userData);
+//   } catch(err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
